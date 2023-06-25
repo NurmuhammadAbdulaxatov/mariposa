a392cfa2ca7aa8076dd29ab9792488fda7d7baf0
NurmuhammadAbdulaxatov <abdulaxatov3@gmail.com>
2023-06-25 18:05:10 +0500

    login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import "./login.scss";
 
 const Login = () => {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <div className="login">
+        <div className="form">
+          <h1>Login</h1>
+          <form className="input-data">
+            <label className="label_username" htmlFor="username">
+              Username
+            </label>
+            <input name="username" id="username" type="text" required />
+
+            <label className="password-label" htmlFor="password">
+              Password
+            </label>
+            <input id="password" name="password" type="password" required />
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
